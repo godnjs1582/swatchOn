5a41a46759d44197e9747ad7ba326096b5f59746
haewon <godnjs13579@naver.com>
2022-06-22 08:42:29 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -4,55 +4,12 @@ import styled from "styled-components";
 
 
 function App() {
-  const [list,setList]=useState([]);
-  let toastProperties =null;
-  const showToast = (type,duration)=>{
-  switch(type){
-    case "success":
-      toastProperties={
-        title:"Success",
-        description:"This is a success toast component",
-        backgroundColor:"#5cb85c",
-        duration:duration
-      }
-    break;
-    case "danger":
-      toastProperties={
-        title:"Danger",
-        description:"This is a danger toast component",
-        backgroundColor:"#d9534f",
-        duration:duration,
-      }
-    break;
-    case "info":
-      toastProperties={
-        title:"Info",
-        description:"This is a info toast component",
-        backgroundColor:"#5bc0de",
-        duration:duration,
-      }
-    break;
-    case "warning":
-      toastProperties={
-        title:"Warning",
-        description:"This is a warning toast component",
-        backgroundColor:"#f0ad4e",
-        duration:duration,
-      }
-    break;
-    default:
-      toastProperties=[];
-    }
-  setList([...list,toastProperties]);
-}
 
-useEffect(()=>{
-  showToast("success",3000)//Toast 띄우기 함수 실행(type,duration 전달)
-},[])
 
   return (
     <Wrapper>
-      <Toast toastList={list} setList={setList}/>
+      <Toast type={"danger"} duration={3000}/> 
+      {/* success,warning,info,danger 4가지 경우와 duration 변경 */}
     </Wrapper>
   );
 }
--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,17 +1,62 @@
-import React ,{useEffect}from 'react';
+import React ,{useEffect,useState}from 'react';
 import styled, { keyframes } from "styled-components";
 
-function Toast({ toastList, setList}) {
+function Toast(props) {
+  const type= props?.type;
+  const duration=props?.duration;
+  const [list,setList]=useState([]);
+  let toastProperties =null;
+  const showToast = (type,duration)=>{
+  switch(type){
+    case "success":
+      toastProperties={
+        title:"Success",
+        description:"This is a success toast component",
+        backgroundColor:"#5cb85c",
+        duration:duration
+      }
+    break;
+    case "danger":
+      toastProperties={
+        title:"Danger",
+        description:"This is a danger toast component",
+        backgroundColor:"#d9534f",
+        duration:duration,
+      }
+    break;
+    case "info":
+      toastProperties={
+        title:"Info",
+        description:"This is a info toast component",
+        backgroundColor:"#5bc0de",
+        duration:duration,
+      }
+    break;
+    case "warning":
+      toastProperties={
+        title:"Warning",
+        description:"This is a warning toast component",
+        backgroundColor:"#f0ad4e",
+        duration:duration,
+      }
+    break;
+    default:
+      toastProperties=[];
+    }
+  setList([toastProperties]);
+}
 
+useEffect(()=>{
+  showToast(type,duration)//Toast 띄우기 함수 실행(type,duration 전달)
+},[])
   
-  const title=toastList[0]?.title; // 토스트 타입
-  const description=toastList[0]?.description;// 토스트 설명
-  const bg=toastList[0]?.backgroundColor;// 토스트 배경색
-  const duration=toastList[0]?.duration;//토스트 지속 시간
+  const title=list[0]?.title; // 토스트 타입
+  const description=list[0]?.description;// 토스트 설명
+  const bg=list[0]?.backgroundColor;// 토스트 배경색
  
   useEffect(()=>{
     const interval =setInterval(()=>{
-      if(toastList.length){
+      if(list.length){
         setList("")
       }
     },duration);//durtaion 이후 setList 안의 값을 비워서 삭제
@@ -19,9 +64,12 @@ function Toast({ toastList, setList}) {
     return ()=>{
       clearInterval(interval);
     }
-  },[toastList])
-
+  },[list])
+useEffect(()=>{
+  showToast(type,duration)
+},[])
   return (
+  
     <Wrapper bg={bg}>
       <Title>{title}</Title>
       <Desc>{description}</Desc>   
